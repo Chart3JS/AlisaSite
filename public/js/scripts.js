0882954c0399916c3002a958752ddbbba9ae6e1a
alexanderplotkin <jprjdev@gmail.com>
2016-04-20 10:34:41 +0300

    SEO+schema.org2

--- public/js/scripts.js
+++ public/js/scripts.js
@@ -543,7 +543,7 @@ function setEqualHeight(columns) {
   var tallestcolumn = 0;
   columns.each(
     function() {
-      currentHeight = $(this).height();
+      var currentHeight = $(this).height();
       if (currentHeight > tallestcolumn) {
         tallestcolumn = currentHeight;
       }
